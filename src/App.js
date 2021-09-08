366385881a01f98f45345b38d87765ff9d548a57
adm@15 <adm@cptsoftware.com.br>
2021-09-08 11:40:50 -0300

    Nova branch
    ambiente de desenvolvimento

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p> Gustavo </p>
+        <p> Gustavo desenvolvedor </p>
         <a
           className="App-link"
           href="https://reactjs.org"
